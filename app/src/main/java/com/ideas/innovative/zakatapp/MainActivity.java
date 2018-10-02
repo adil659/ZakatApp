4395a33c14e70014a2ed7759906ea706d4383c59
Adil <aughr063@uottawa.ca>
2018-10-01 23:04:11 -0400

    first commit

---
+++ app/src/main/java/com/ideas/innovative/zakatapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ideas.innovative.zakatapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
