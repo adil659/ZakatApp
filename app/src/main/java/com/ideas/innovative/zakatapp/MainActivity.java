143292cc6a966b2f46a2f75956f09e1a31a2595e
Adil <aughr063@uottawa.ca>
2018-10-27 12:00:02 -0400

    Fixed up luqman's UI work

--- app/src/main/java/com/ideas/innovative/zakatapp/MainActivity.java
+++ app/src/main/java/com/ideas/innovative/zakatapp/MainActivity.java
@@ -1,13 +1,24 @@
 package com.ideas.innovative.zakatapp;
 
+import android.content.Context;
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+    @Override
+    public View onCreateView(View parent, String name, Context context, AttributeSet attrs) {
+        return super.onCreateView(parent, name, context, attrs);
+    }
+
 }
